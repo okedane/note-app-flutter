cc880e344a63c5d54e67399e905c2298f9f0c631
dhanie <romadani.code@gmail.com>
2025-09-14 18:03:15 +0700

    update ui

---
+++ lib/core.dart
@@ -0,0 +1,13 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+fileCount: 7
+*/
+export 'package:note_app/core_package.dart';
+export 'package:note_app/presentation/home/controller/home_controller.dart';
+export 'package:note_app/presentation/home/view/home_view.dart';
+export 'package:note_app/presentation/login/controller/login_controller.dart';
+export 'package:note_app/presentation/login/view/login_view.dart';
+export 'package:note_app/presentation/splash/controller/splash_controller.dart';
+export 'package:note_app/presentation/splash/view/splash_view.dart';
